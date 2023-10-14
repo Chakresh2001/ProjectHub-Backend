610620279b8f9012e887d1200365fc1891827401
chakresh2001 <jha.chakresh2001@gmail.com>
2023-10-14 13:18:03 +0530

    done

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ app.use(express.json())
 app.use(cors())
 app.use("/", Router)
 
+const dotenv = require('dotenv');
 
-app.listen(8080, async () => {
-    await mongoose.connect("mongodb+srv://chakresh1234:chakresh1234@cluster0.cqppmvp.mongodb.net/ProjectHub?retryWrites=true&w=majority")
-    console.log("server started on 8080")
+dotenv.config();
+
+
+app.listen(process.env.PORT, async () => {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log("server started ...")
 })
